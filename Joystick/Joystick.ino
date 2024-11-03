1a971073d17fd5cb92a8abeff1a9942b8caad9e3
Natthawut-Ch <150988991+AllCodeCraft-Ch@users.noreply.github.com>
2024-11-03 15:23:59 +0700

    Update Joystick.ino

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,4 +1,4 @@
-#include<Servo.h>
+#include <Servo.h>
 
 #define x_pin A0
 #define y_pin A1
@@ -10,21 +10,16 @@
 Servo servo_x;
 Servo servo_y;
 
-int pos_x = 180;
-int pos_y = 180;
+int pos_x = 180; // เริ่มต้นที่ค่ากลาง (90 องศา)
+int pos_y = 180; // เริ่มต้นที่ค่ากลาง (90 องศา)
 
 void setup() {
   pinMode(x_pin, INPUT);
   pinMode(y_pin, INPUT);
   pinMode(sw_pin, INPUT);
 
-  pinMode(ServoX_Pin, OUTPUT);
-  pinMode(ServoY_Pin, OUTPUT);
-
   servo_x.attach(ServoX_Pin);
-  servo_x.attach(pos_x);
   servo_y.attach(ServoY_Pin);
-  servo_x.attach(pos_y)
 
   delay(1000);
 }
@@ -32,14 +27,13 @@ void setup() {
 void loop() {
   int x_data = analogRead(x_pin);
   int y_data = analogRead(y_pin);
-  int sw_pin = digitalRead(sw_pin);
+  int sw_state = digitalRead(sw_pin); // กำหนดตัวแปรใหม่แทน `sw_pin` เพื่อไม่ให้ซ้ำกับพินที่ประกาศด้านบน
 
-  pos_x = map(x_data,0,1023,0,180);
-  pos_y = map(x_data,0,1023,0,180);
+  pos_x = map(x_data, 0, 1023, 0, 180);
+  pos_y = map(y_data, 0, 1023, 0, 180); // แก้ให้ใช้ `y_data` แทน `x_data` สำหรับการควบคุมเซอร์โวแกน Y
 
   servo_x.write(pos_x);
   servo_y.write(pos_y);
 
   delay(40);
-
 }
